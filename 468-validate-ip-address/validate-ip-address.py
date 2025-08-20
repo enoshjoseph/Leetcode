115f7fe1c921b3bc5a83d3aecf7951c156ccab7e
enoshjoseph <140941715+enoshjoseph@users.noreply.github.com>
2025-08-20 14:54:22 +0530

    Time: 2 ms (5.67%) | Memory: 17.9 MB (44.93%) - LeetSync

---
+++ 468-validate-ip-address/validate-ip-address.py
@@ -0,0 +1,19 @@
+import re
+class Solution:
+    def validIPAddress(self, queryIP: str) -> str:
+        v4 = re.compile(
+                    r"((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])\.){3}"
+                    r"(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])"
+                    )
+
+                
+        v6 = re.compile(r"([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}")
+
+        if v4.fullmatch(queryIP):
+            return "IPv4"
+        elif v6.fullmatch(queryIP):
+            return "IPv6"
+
+        else:
+            return "Neither"
+        
