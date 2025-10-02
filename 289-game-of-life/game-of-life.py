1c47372a13f4576a84c93fad578dc47b564e5c5c
enoshjoseph <140941715+enoshjoseph@users.noreply.github.com>
2025-10-02 14:09:16 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (75.72%) - LeetSync

--- 289-game-of-life/game-of-life.py
+++ 289-game-of-life/game-of-life.py
@@ -6,22 +6,27 @@ class Solution:
 
         rows = len(board)
         cols = len(board[0])
-        
-        copy_board = [[board[row][col] for col in range(cols)] for row in range(rows)]
-
         for r in range(rows):
             for c in range(cols):
                 count = 0
-                curr = [(r+1, c), (r-1, c),(r, c+1), (r, c-1), (r+1, c+1), (r-1, c-1), (r-1, c+1), (r+1, c-1)]
 
-                for i,j in curr:
-                    if i >= 0 and j >= 0 and i<rows and j < cols and copy_board[i][j] == 1:
+                curr = [(r+1, c), (r-1, c),(r, c+1), (r, c-1), (r+1, c+1), (r - 1, c+1), (r - 1, c - 1), (r+1, c-1) ]
+
+                for i, j in curr:
+                    if i >=0 and j>=0 and i<rows and j < cols and (board[i][j] == 1 or board[i][j] == 2):
                         count += 1
-
-                if copy_board[r][c] == 1:
-                    if count < 2 or count > 3:
-                        board[r][c] = 0
                 
-                else:
-                    if count == 3:
-                        board[r][c] = 1
+                if board[r][c] == 1 and (count <2 or count > 3):
+                    board[r][c] = 2
+                
+                elif board[r][c] == 0 and count == 3:
+                    board[r][c] = -1
+
+        
+        for row in range(rows):
+            for col in range(cols):
+                if board[row][col] == 2:
+                    board[row][col] = 0
+                elif board[row][col] == -1:
+                    board[row][col] = 1
+    
