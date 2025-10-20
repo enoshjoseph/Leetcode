0cf27c1e7879bbf3e98339dc6ee162963ae4586c
enoshjoseph <140941715+enoshjoseph@users.noreply.github.com>
2025-10-20 16:59:17 +0530

    Time: 8 ms (27.82%) | Memory: 17.7 MB (95.29%) - LeetSync

--- 2819-remove-trailing-zeros-from-a-string/remove-trailing-zeros-from-a-string.py
+++ 2819-remove-trailing-zeros-from-a-string/remove-trailing-zeros-from-a-string.py
@@ -1,7 +1,7 @@
 class Solution:
     def removeTrailingZeros(self, num: str) -> str:
         i = len(num) - 1
-        while num[i] == "0" and num[i - 1]:
+        while num[i] == "0" and (i - 1 >= 0):
             i -= 1
         return num[:i+1]
 
