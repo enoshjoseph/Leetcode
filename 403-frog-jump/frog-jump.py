e1c5bb6ac57b324e1562909a8082e6b24e6097dc
enoshjoseph <140941715+enoshjoseph@users.noreply.github.com>
2025-10-20 16:08:53 +0530

    Time: 99 ms (61.12%) | Memory: 19.3 MB (89.16%) - LeetSync

---
+++ 403-frog-jump/frog-jump.py
@@ -0,0 +1,10 @@
+class Solution:
+    def canCross(self, stones: List[int]) -> bool:
+       dp = {stone : set() for stone in stones}
+       dp[0].add(0)
+       for stone in stones:
+        for j in dp[stone]:
+            for step in [j-1, j, j+1]:
+                if step > 0 and (step + stone) in dp:
+                    dp[step +stone].add(step)
+       return len(dp[stones[-1]]) > 0
